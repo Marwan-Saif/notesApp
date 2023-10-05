a8850841a91e04d4829fad6ff4a78ff2b259c146
marwan <merosaif837@gmail.com>
2023-10-05 00:54:57 -0700

    create featchNotes method

--- lib/cubits/notes_cubit.dart/notes_cubit.dart
+++ lib/cubits/notes_cubit.dart/notes_cubit.dart
@@ -1,8 +1,21 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
+import 'package:notes_app/constant.dart';
 
 import 'package:notes_app/models/note_model.dart';
 part 'notes_state.dart';
 
 class AddNoteCubit extends Cubit<NotesState> {
   AddNoteCubit() : super(NotesInitial());
+
+  featchAllNotes() async {
+    try {
+      emit(Notesloading());
+      var notesBox = Hive.box(kNotesBox);
+      List<NoteModel> notes = notesBox.values.toList() as List<NoteModel>;
+      emit(NotesSuccess(notes: notes));
+    } catch (e) {
+      emit(NotesFaliure(e.toString()));
+    }
+  }
 }
