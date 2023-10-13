e318cb34917d413154fafdaf107b3e8188c2e432
marwan <merosaif837@gmail.com>
2023-10-13 04:48:15 -0700

    select color of note on creation

--- lib/components/colors_listview.dart
+++ lib/components/colors_listview.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 
 class ColorItem extends StatelessWidget {
   const ColorItem({
@@ -54,6 +56,7 @@ class _ColorsListViewState extends State<ColorsListView> {
         itemBuilder: (context, index) => GestureDetector(
           onTap: () {
             currentItem = index;
+            BlocProvider.of<AddNoteCubit>(context).color = colors[currentItem];
             setState(() {});
           },
           child: ColorItem(
--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:notes_app/constant.dart';
@@ -6,9 +7,10 @@ part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
-
+  Color color = const Color(0xFFF6EEE0);
   addNote(NoteModel note) async {
     try {
+      note.color = color.value;
       emit(AddNoteloading());
       var notesBox = Hive.box<NoteModel>(kNotesBox);
       await notesBox.add(note);
